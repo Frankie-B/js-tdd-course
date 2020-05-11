303e0e93d88d74323969d4313ecf370f845ccfbf
Francis Bukenya <frankie.bukenya@gmail.com>
2020-05-11 12:34:44 +0200

    Add working endpoint test to server

--- tdd-integration-testing/tdd-server/src/server.test.js
+++ tdd-integration-testing/tdd-server/src/server.test.js
@@ -2,8 +2,10 @@ import sinon from 'sinon';
 import request from 'supertest';
 import { expect } from 'chai';
 import db from './db';
+import { app } from './server';
 
-describe('GET /user/:username', () => {
+// integration test for a server endpoint.
+describe('GET /users/:username', () => {
   it('should send the correct response when a user with the correct username is found', async () => {
     //setup some fake data to use
     const fakeData = {
@@ -12,10 +14,17 @@ describe('GET /user/:username', () => {
       email: 'hello@world.com',
     };
 
-    //create a STUB with sinon
     const stub = sinon.stub(db, 'getUserByUsername').resolves(fakeData);
+    // using supertest to test server endpoints.
+    await request(app)
+      .get('/users/hello')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(fakeData);
+    // check to see if your stub was called
+    expect(stub.getCall(0).args[0]).to.equal('hello');
 
     // always remember to call the stub in our tests
-    stub.restore();
+    stub.restore(); // returns method to original state before any other tests are run.
   });
 });
