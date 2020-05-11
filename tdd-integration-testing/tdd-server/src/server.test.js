7ea6d0ae3b194438722276022e83f70c5d019572
Francis Bukenya <frankie.bukenya@gmail.com>
2020-05-11 09:27:50 +0200

    Add stun to server test

--- tdd-integration-testing/tdd-server/src/server.test.js
+++ tdd-integration-testing/tdd-server/src/server.test.js
@@ -4,5 +4,18 @@ import { expect } from 'chai';
 import db from './db';
 
 describe('GET /user/:username', () => {
-  it('should send the correct response when a user with the correct username is found', async () => {});
+  it('should send the correct response when a user with the correct username is found', async () => {
+    //setup some fake data to use
+    const fakeData = {
+      id: '1357',
+      username: 'hello',
+      email: 'hello@world.com',
+    };
+
+    //create a STUB with sinon
+    const stub = sinon.stub(db, 'getUserByUsername').resolves(fakeData);
+
+    // always remember to call the stub in our tests
+    stub.restore();
+  });
 });
